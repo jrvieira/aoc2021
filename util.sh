c3492733ee8fc21663c4adfa3694c6b7e91427ea
jrvieira <github@jrvieira.com>
2021-11-28 23:02:38 +0000

    tooling

--- util.sh
+++ util.sh
@@ -2,14 +2,26 @@
 
 aoc () {
 
-   if [ "$1" == "fetch" ]; then
+   if [ "$1" = "fetch" ]; then
       ./fetch $2 $3
-   elif [ "$1" == "test" ]; then
-      ghcid --warnings --lint --no-status --test-message="" --clear --no-height-limit --test=":main $2 test"
-   elif [ "$1" == "run" ]; then
+   elif [ "$1" = "test" ]; then
+      ghcid --warnings --lint --no-status --clear --no-height-limit --test=":main $2 test" --test-message="
+"
+   elif [ "$1" = "run" ]; then
       cabal run -v0 -O2 aoc2021 $2
+   elif [[ -z $1 || "$1" = "help" ]]; then
+      echo -e "  "
+      echo -e "  \033[0;33m# fetch daily input\033[0m"
+      echo -e "  aoc fetch [{d}] [{y}]"
+      echo -e "  "
+      echo -e "  \033[0;33m# daemonize daily tests\033[0m"
+      echo -e "  aoc test {d}"
+      echo -e "  "
+      echo -e "  \033[0;33m# compile and run optimized solution\033[0m"
+      echo -e "  aoc run {d}"
+      echo -e "  "
    else
-      echo "error: invalid command"
+      echo "error: invalid command \"$1\""
    fi
 
 }
