858ccb704313380e47944469966fff0c464a84af
jrvieira <github@jrvieira.com>
2021-12-02 00:35:28 +0000

    day 1

--- src/Α01.hs
+++ src/Α01.hs
@@ -1,26 +1,29 @@
 module Α01 where
 
 import Zero.Zero
+import Data.List
+import Control.Arrow ( (&&&) )
 
 test :: IO ()
 test = do
    input <- parse <$> readFile "./tests/01.txt"
-   teqt "part 1" "" $ part1 input
--- teqt "part 2" "" $ part2 input
+   teqt "part 1" 7 $ part1 input
+   teqt "part 2" 5 $ part2 input
 
 main :: IO ()
 main = do
    input <- parse <$> readFile "./input/01.txt"
    print $ part1 input
--- print $ part2 input
+   print $ part2 input
 
-parse = id
+parse :: String -> [Int]
+parse = map read . lines
 
 -- part 1
 
-part1 = id
+part1 = length . filter id . uncurry (zipWith (<)) . (id &&& tail)
 
 -- part 2
 
---part2 :: _
+part2 = part1 . map (sum . take 3) . tails
 
