6f30e9aa280ebeeca7482d109532e4e249d9f9fb
jrvieira <github@jrvieira.com>
2021-12-09 01:25:25 +0000

    day 7

--- src/Η07.hs
+++ src/Η07.hs
@@ -1,25 +1,31 @@
 module Η07 where
 
 import Zero.Zero
+import Control.Arrow
 
 test :: IO ()
 test = do
    input <- parse <$> readFile "./tests/07.txt"
-   teqt "part 1" "" $ part1 input
--- teqt "part 2" "" $ part2 input
+   teqt "part 1" 37 $ part1 input
+   teqt "part 2" 168 $ part2 input
 
 main :: IO ()
 main = do
    input <- parse <$> readFile "./input/07.txt"
    print $ part1 input
--- print $ part2 input
+   print $ part2 input
 
-parse = id
+parse = parseNums
 
 -- part 1
 
-part1 = id
+part1 cs = minimum $ fuel <$> [minimum cs..maximum cs]
+   where
+   fuel x = sum $ abs . (x-) <$> cs
 
 -- part 2
 
-
+part2 cs = minimum $ fuel <$> [minimum cs..maximum cs]
+   where
+   fuel x = sum $ tri . abs . (x-) <$> cs
+   tri x = div (x * succ x) 2
