56abedd8d5911c100277f6a9a947b1e882fe343c
jrvieira <github@jrvieira.com>
2022-08-25 22:43:57 +0100

    some something

--- src/Ρ17.hs
+++ src/Ρ17.hs
@@ -2,6 +2,10 @@ module Ρ17 where
 
 import Zero.Zero
 
+import Data.List ( lookup )
+import Data.Bifunctor ( bimap )
+import Data.Maybe ( mapMaybe )
+
 test :: IO ()
 test = do
    input <- parse <$> readFile "./tests/17.txt"
@@ -16,9 +20,33 @@ main = do
 
 parse = id
 
--- part 1
-
 part1 = id
+
+{- axioms:
+
+-- final x ∈ T
+-- x = n in Tn <- final x
+-- where Tn = triangular number
+
+-}
+
+tn :: Num a => a -> Maybe a
+tn x = lookup (abs x) $ takeUntil (> x) triangulars
+   where
+   triangulars = zip (triangular <$> [0..]) [0..]
+   triangular n = div (succ n * n) 2
+
+possibleTargetsX :: Num a => [a] -> [a]
+possibleTargetsX = mapMaybe tn
+
+step (x,y) = bimap (drag . (+ x)) (gravity . (+ y)) (x,y)
+   where
+   gravity = pred
+   drag
+      | x > 0 = first pred
+      | x < 0 = first succ
+      | otherwise = id
+
 
 -- part 2
 
