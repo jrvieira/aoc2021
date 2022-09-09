9bca869d244a94290991969265f60dc5987811aa
jrvieira <github@jrvieira.com>
2022-09-09 11:35:46 +0100

    day 19

--- src/Τ19.hs
+++ src/Τ19.hs
@@ -2,14 +2,14 @@ module Τ19 where
 
 import Zero.Zero
 
-import Data.List ( tails )
+import Data.List ( partition )
 import Data.List.Split ( splitOn )
 
 test :: IO ()
 test = do
    input <- parse <$> readFile "./tests/19.txt"
-   teqt "part 1" mempty $ part1 input
--- teqt "part 2" "" $ part2 input
+   teqt "part 1" 79 $ part1 input
+-- teqt "part 2" mempty $ part2 input
 
 main :: IO ()
 main = do
@@ -29,21 +29,38 @@ point s = Point x y z where (x,y,z) = read ('(' : s <> ")") :: (Val,Val,Val)
 
 -- part 1
 
--- here scanners are identified by the distances between all pairs of beacons in range
-part1 :: [[Point]] -> [[Val]]
-part1 = map distances
+-- beacons are lists of distances
+part1 :: [[Point]] -> Int
+part1 = length . go [] . concatMap beacons
+   where
+   go acc [] = acc
+   go acc (b:bs)
+      | False  # show (length as) = undefined  -- should always be 0
+      | Just a <- match ???
+      where
+      (as,as') = partition (isJust $ match b) acc
 
-adjacent :: [Val] -> Bool
-adjacent = undefined
+newtype Beacon = Beacon { δ :: [[Val]] }
 
-distances :: [Point] -> [Val]
-distances = map (uncurry distance) . pairs
+instance Semigroup Beacon where
+   Beacon a <> Beacon b = Beacon (a <> b)
+
+match :: Beacon -> Beacon -> Maybe Beacon
+match (Beacon a) (Beacon b)
+   | any (uncurry $ (>= 12) . length , (∩)) $ (,) <$> a <*> b = Just $ Beacon (a <> b)
+   | otherwise = Nothing
+
+-- a beacon is defined by the distances between itself and surrounding beacons
+-- implementation detail: each beacon keeps a 0.0 distance to itself
+beacons :: [Point] -> [Beacon]
+beacons ps = map (Beacon . (: [])) $ go [] ps
+   where
+   go acc [] = acc
+   go acc (x:xs) = go ((distance x <$> ps) : acc) xs
 
 distance :: Point -> Point -> Val
-distance a b = sqrt ( (x b - x a)^2 + (x b - x a)^2 + (z b - z a)^2 )
+distance a b = sqrt ( (x b - x a)^2 + (y b - y a)^2 + (z b - z a)^2 )
 
-pairs :: [a] -> [(a,a)]
-pairs l = [ (a,b) | (a:bs) <- tails l , b <- bs ]
 
 -- part 2
 
